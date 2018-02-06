986c9f54aae19e1ac6f606e78594b5809db27a4c
Tomasz Lenarcik <apendua@gmail.com>
2018-02-06 23:27:18 +0100

    Add script for creating proxy modules

---
+++ post-build.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const mkdirp = require('mkdirp'); // eslint-disable-line
+
+[
+  'plugins',
+  'presets',
+].forEach((name) => {
+  mkdirp(name, () => {
+    fs.readdir(`./lib/${name}`, (err, files) => {
+      if (err) {
+        console.error(err); // eslint-disable-line
+      }
+      files.forEach((file) => {
+        console.log(`create file: ${name}/${file}`); // eslint-disable-line
+        fs.writeFileSync(`./${name}/${file}`, `module.exports = require('../lib/${name}/${file}');\n`);
+      });
+    });
+  });
+});
+
+[
+  'Compiler',
+  'Scope',
+].forEach((name) => {
+  console.log(`create file: ${name}.js`); // eslint-disable-line
+  fs.writeFileSync(`./${name}.js`, `module.exports = require('../lib/${name}.js');\n`);
+});
