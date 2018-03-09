22d04baf60924db33b51c2043f62f4010eeafcef
Tomasz Lenarcik <apendua@gmail.com>
2018-03-09 16:35:53 +0100

    Add phone numbers list to the example form

--- examples/complex-form/src/containers/Form.js
+++ examples/complex-form/src/containers/Form.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import map from 'lodash/map';
 import { Form as F } from '../store/Context';
 
 const Input = F.connect({
@@ -15,28 +17,88 @@ const Input = F.connect({
   <input value={(props.edited ? props.value : props.defaultValue) || ''} onChange={props.onChange} />
 ));
 
-const Field = F.connect({
+const Select = F.connect({
   value: '$state.value',
+  edited: '$state.edited',
+}, {
+  onChange: ({ edited, $set }) => (e) => {
+    $set('value', e.target.value);
+    if (!edited) {
+      $set('edited', true);
+    }
+  },
 })(props => (
-  <F.Section section={props.name} reducer="field">
-    <Input defaultValue={props.defaultValue} />
-  </F.Section>
+  <select value={(props.edited ? props.value : props.defaultValue) || ''} onChange={props.onChange}>
+    {map(props.options, ({
+      value,
+      label,
+    }) => (
+      <option key={value} value={value}>
+        {label}
+      </option>
+    ))}
+  </select>
 ));
+
+const Field = ({ name, component: Component, ...props }) => (
+  <F.Section section={name} reducer="field">
+    <div>
+      <span>{name}</span>
+      <Component {...props} />
+    </div>
+  </F.Section>
+);
+
+Field.propTypes = {
+  component: PropTypes.func,
+  name: PropTypes.string.isRequired,
+  defaultValue: PropTypes.string,
+};
+
+Field.defaultProps = {
+  defaultValue: null,
+  component: Input,
+};
+
+const typeOptions = [
+  { value: '', label: 'Default' },
+  { value: 'Mobile', label: 'Mobile' },
+  { value: 'Work', label: 'Work' },
+];
 
 const Form = F.connect({
   a: '$state.a.value',
   b: '$state.b.value',
   c: { $sum: ['$a', '$b'] },
+  phones: '$state.phones',
+}, {
+  onAppend: ({ $push }) => () => {
+    $push('phones', {});
+  },
 })(props => (
-  <form>
+  <div>
     <Field name="a" />
     <Field name="b" />
     <Field name="c" defaultValue={props.c} />
-  </form>
+    {map(props.phones, (phone, index) => (
+      <F.Section key={index} section={`phones.${index}`}>
+        <Field name="number" />
+        <Field name="type" component={Select} options={typeOptions} />
+      </F.Section>
+    ))}
+    <button onClick={props.onAppend}>
+      Append
+    </button>
+  </div>
 ));
 
 export default () => (
-  <F.Section section="form">
-    <Form />
-  </F.Section>
+  <Fragment>
+    <F.Section section="form">
+      <Form />
+    </F.Section>
+    <F.Section section="form2">
+      <Form />
+    </F.Section>
+  </Fragment>
 );
