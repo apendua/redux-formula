f5e98892ef20fbb73b927b9c695a41f71a332172
Tomasz Lenarcik <apendua@gmail.com>
2018-03-16 14:57:58 +0100

    Remove unnecessary complexity at reference plugin

--- src/Compiler/plugins/reference.js
+++ src/Compiler/plugins/reference.js
@@ -1,20 +1,15 @@
 import isPlainObject from 'lodash/isPlainObject';
 import has from 'lodash/has';
-import get from 'lodash/get';
-import { split } from '../../utils';
 
 const pluginReference = {
   createCompiler: () => next => (expression) => {
     if (!isPlainObject(expression) || !has(expression, '$')) {
       return next(expression);
     }
-    const [name, dataKey] = split(expression.$);
+    const name = expression.$;
     return {
       deps: { [name]: name },
-      bindTo: scope => scope.boundSelector(
-        scope.resolve(name).selector,
-        value => (dataKey ? get(value, dataKey) : value),
-      ),
+      bindTo: scope => scope.relative(scope.resolve(name).selector),
     };
   },
 };
