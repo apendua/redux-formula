45c8d626402fb513ba49422ea11288a0e5059b44
Tomasz Lenarcik <apendua@gmail.com>
2018-03-19 10:10:36 +0100

    Use unicode arrow to mark token at error message

--- src/errors.js
+++ src/errors.js
@@ -3,16 +3,16 @@ const format = (message, details) => {
   if (details) {
     const newMessage = `${message} // at ${details.line + 1}:${details.from + 1}`;
     if (details.lineContent) {
-      let ruler = '';
-      while (ruler.length < details.from) {
-        ruler += ' ';
-      }
-      while (ruler.length <= details.to) {
-        ruler += '^';
-      }
-      return `${newMessage}
-${details.lineContent}
-${ruler}`;
+      // let ruler = '';
+      // while (ruler.length < details.from) {
+      //   ruler += ' ';
+      // }
+      // while (ruler.length <= details.to) {
+      //   ruler += '^';
+      // }
+      const tokenPosition = details.lineContent
+        .split('').map((c, i) => (details.from === i ? `${c}\u0362` : c)).join('');
+      return `${newMessage}\n${tokenPosition}\n`;
     }
     return newMessage;
   }
